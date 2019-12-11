e53f8de97a87d05cef8529ac6e7b002f2d5fe691
caesar <newman0934@gmail.com>
2019-12-11 10:20:50 +0800

    建立假資料

--- models/product.js
+++ models/product.js
@@ -1,26 +1,32 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Product = sequelize.define('Product', {
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    price: DataTypes.INTEGER,
-    image: DataTypes.STRING
-  }, {});
+  const Product = sequelize.define(
+    "Product",
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      price: DataTypes.INTEGER,
+      image: DataTypes.STRING
+    },
+    {}
+  );
   Product.associate = function(models) {
     Product.belongsToMany(models.Cart, {
-      as:"carts",
-      through:{
-        model: models.CartItem, unique: false
-      },
-      foreignKey: "ProductId"
-    })
-    Product.belongsToMany(model.Order, {
-      as:"orders",
+      as: "carts",
       through: {
-        model:models.OrderItem, unique: false
+        model: models.CartItem,
+        unique: false
       },
       foreignKey: "ProductId"
-    })
+    });
+    Product.belongsToMany(model.Order, {
+      as: "orders",
+      through: {
+        model: models.OrderItem,
+        unique: false
+      },
+      foreignKey: "ProductId"
+    });
   };
   return Product;
 };
